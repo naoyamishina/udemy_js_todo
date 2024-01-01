611ff0e819db69bf05651c414c19ef5239bbce38
mishina naoya <msnyxx811@gmail.com>
2024-01-01 17:01:50 +0900

    戻す処理

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  // 未完了リストに追加
+  createIncompleteTodo(inputText);
+};
+
+// 渡された引数を基に未完了のTODOを作成する関数
+const createIncompleteTodo = (todo) => {
   // li生成
   const li = document.createElement("li");
 
@@ -13,7 +19,7 @@ const onClickAdd = () => {
   // p生成
   const p = document.createElement("p");
   p.className = "todo-item";
-  p.innerText = inputText;
+  p.innerText = todo;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -26,19 +32,26 @@ const onClickAdd = () => {
     // 戻すボタンを生成してdivタグ配下に設定
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // TODOの内容を取得し、未完了リストに追加
+      const todoText = backButton.previousElementSibling.innerText;
+      createIncompleteTodo(todoText);
+      // 押された戻すボタンの親にあるliタグを削除
+      backButton.closest("li").remove();
+    });
     moveTarget.firstElementChild.appendChild(backButton);
     // 完了リストに移動
     document.getElementById("complete-list").appendChild(moveTarget);
-  })
+  });
 
   // button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    // 押された削除ボタンの親にあるliタグ配下を削除
-    const removeTarget = deleteButton.closest("li");
-    document.getElementById("incomplete-list").removeChild(removeTarget)
-  })
+    // 押された削除ボタンの親にあるliタグを未完了リストから削除
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   // liタグの子要素に各要素を設定
   div.appendChild(p);
